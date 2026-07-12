f4adf8f1664451ac41a8e16dd9f47730637f6113
thomas <thomaslazzeri3@gmail.com>
2026-07-12 19:44:51 +0200

    add Header and Main components with initial styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,4 +2,14 @@ import { useState } from 'react'
 import './App.css'
 
 
+const app = () => {
+  const [state, setState] = useState(0);
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+};
+
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import './Header.css';
+
+export const Header = (props) => (
+    <header>
+        <div>
+            <h1>Learn Web development</h1>
+        </div>
+    </header>
+);
